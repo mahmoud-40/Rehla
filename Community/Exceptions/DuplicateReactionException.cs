a6e955a902d364531ac11dbe878047ee0558e9ec
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2026-06-06 22:21:47 +0300

    feat: update post dto and add duplicate reaction exception

---
+++ Community/Exceptions/DuplicateReactionException.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace BreastCancer.Community.Exceptions
+{
+    [Serializable]
+    public class DuplicateReactionException : Exception
+    {
+        public DuplicateReactionException( string message) : base(message) { }
+
+        private DuplicateReactionException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
