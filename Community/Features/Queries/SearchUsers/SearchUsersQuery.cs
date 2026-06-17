81c2a3a118a2c142de797403cc0baa9fb563cf82
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2026-06-18 01:40:25 +0300

    feat: add SearchUsersQuery and UserSearchDto

---
+++ Community/Features/Queries/SearchUsers/SearchUsersQuery.cs
@@ -0,0 +1,7 @@
+﻿using BreastCancer.Community.DTO.response;
+using MediatR;
+
+namespace BreastCancer.Community.Features.Queries.SearchUsers
+{
+    public record SearchUsersQuery(string SearchTerm, int Limit = 20) : IRequest<List<UserSearchDto>>;
+}
